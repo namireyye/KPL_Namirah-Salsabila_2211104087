73b79960d8827976dd189295e36e4fec1ee539b5
“Namirah <namirahsalsabilla@gmail.com>
2025-03-18 14:19:20 +0700

    first commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Namirah Salsabila"
+
+function getMyIdentity() {
+    let localVariable = 2211102016
+
+    console.log(`Nama saya ${ globalVarible }nim ${ localVariable }`)
+}
+
+console.log(globalVarible)
+    // console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,14 @@
+var name = "Namirah Salsabila"
+const nim = 2211104087
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`nama saya adalah ${ name } dan nim saya adalah ${ nim }`)
+
+console.log(`Nama saya adalah ${ name } nim saya adalah ${ nim }`)
+
+
+var name = "ayam"
+
+let fullName = "Muhammad Rizqi"
