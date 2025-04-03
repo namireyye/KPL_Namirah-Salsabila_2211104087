c1bc7048b3c07e159ac9c90672e8691ebab40f33
“Namirah <namirahsalsabilla@gmail.com>
2025-04-03 19:59:08 +0700

    menambahkan class DataGeneric

---
+++ 05_2211104087/tpmodul5_2211104087.js
@@ -0,0 +1,33 @@
+// A. Membuat class dengan nama "HaloGeneric"
+class HaloGeneric {
+    SapaUser(user) {
+      console.log(Halo user ${user});
+    }
+  }
+  
+  // Tambahan sesuai instruksi gambar:
+  // Membuat class DataGeneric dengan generic tipe T
+  class DataGeneric {
+    constructor(data) {
+      this.data = data; // menyimpan data bertipe generik T
+    }
+  
+    // Method PrintData untuk menampilkan nilai data
+    PrintData() {
+      console.log(Data yang tersimpan adalah: ${this.data});
+    }
+  }
+  
+  // Fungsi utama
+  function main() {
+    const halo = new HaloGeneric();
+    halo.SapaUser("Namirah");
+  
+    // C. Mengisi data dengan NIM dan memanggil PrintData
+    const nim = “2211104087”;
+    const data = new DataGeneric(nim);
+    data.PrintData();
+  }
+  
+  // Menjalankan fungsi utama
+  main();
